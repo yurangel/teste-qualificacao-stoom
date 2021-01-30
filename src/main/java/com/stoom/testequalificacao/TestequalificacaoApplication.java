ba3b8f3e63eeaeaefab055a4c9cbcaf355aafcf8
Yury Jose Sacilotto <yury_joey@hotmail.com>
2021-01-30 16:45:48 -0300

    open new project to test

---
+++ src/main/java/com/stoom/testequalificacao/TestequalificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.stoom.testequalificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestequalificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestequalificacaoApplication.class, args);
+	}
+
+}
